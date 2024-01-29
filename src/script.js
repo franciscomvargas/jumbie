5cc4c4d524a5aff0e05e2ef28da55ce65e7ea1b5
franciscomvargas <87917356+franciscomvargas@users.noreply.github.com>
2024-01-29 04:46:41 +0000

    CLN: pretify header and more animations

--- src/script.js
+++ src/script.js
@@ -10,51 +10,41 @@ const l5_color = '#D18FB2';
 
 function dark_theme() {
     $('body').css('background-color', dark_color);
-    $('header').css('background-color', dark_color);
+    // $('header').css('background-color', 'blue');
 
     $('body').css('color', white_color);
-    $('.nav-link').css('background-color', white_color);
+    // $('.nav-link').css('background-color', white_color);
 
-    $('.nav-home').css('background-color', white_color);
-    $('.nav-about').css('background-color', white_color);
-    $('.nav-services').css('background-color', white_color);
-    $('.nav-portfolio').css('background-color', white_color);
-    $('.nav-contacts').css('background-color', white_color);
+    $('.nav-home, .nav-about, .nav-services, .nav-portfolio, .nav-contacts').css('background-color', white_color);
 }
 function light_theme() {
     $('body').css('background-color', white_color);
-    $('header').css('background-color', white_color);
+    $('header').css('background-color', dark_color);
 
     $('body').css('color', dark_color);
-    $('.nav-link').css('background-color', dark_color);
+    // $('.nav-link').css('background-color', dark_color);
     
-    $('.nav-home').css('background-color', dark_color);
-    $('.nav-about').css('background-color', dark_color);
-    $('.nav-services').css('background-color', dark_color);
-    $('.nav-portfolio').css('background-color', dark_color);
-    $('.nav-contacts').css('background-color', dark_color);
+    $('.nav-home, .nav-about, .nav-services, .nav-portfolio, .nav-contacts').css('background-color', white_color);
 }
-function home_replace(toHome){
-    if (toHome){
-        $('.nav-home').css('visibility', 'visible');
-        $('.company-logo').css('visibility', 'hidden');
-    } 
-    else {
-        $('.nav-home').css('visibility', 'hidden');
-        $('.company-logo').css('visibility', 'visible');
-    }
-}
+// function home_replace(toHome){
+//     if (toHome){
+//         $('.company-logo').css('visibility', 'hidden');
+//     } 
+//     else {
+//         $('.company-logo').css('visibility', 'visible');
+//     }
+// }
 
 $('.nav-home, .company-logo').on( "click", function() {
     dark_theme();
-    home_replace(true)
+    // home_replace(true)
     $('.nav-home').css('background-color', l1_color);
     $('.section-homepage').css('display', 'block');
     $('.section-about').css('display', 'none');
 })
 $('.nav-about').on( "click", function() {
     light_theme();
-    home_replace(false)
+    // home_replace(false)
     $('.nav-about').css('background-color', l2_color);
     $('.section-homepage').css('display', 'none');
     $('.section-about').css('display', 'block');
