ea408d57ccb41e39e584b8c4a0e387e88d4fa70f
franciscomvargas <87917356+franciscomvargas@users.noreply.github.com>
2024-02-21 10:10:48 +0000

    FIX: Top Left Logo ~ display (mobile navbar)

--- src/script.js
+++ src/script.js
@@ -37,10 +37,12 @@ function hide_dropdown_nav(from_trigger=false) {
     if($('.navTrigger').css('display') === 'block'){
         $('.navTrigger').toggleClass('active');
         if($('.navTrigger').hasClass('active')){
-            console.log("NavBar Exists!!")
+            console.log("NavBar Exists!!");
+            $('div.logo').css('display', 'none')
             $('.main_list').css('overflow', 'visible');
         } else {
             console.log("NavBar ! Exists!!")
+            $('div.logo').css('display', 'block')
             $('.main_list').css('overflow', 'hidden')
         }
         $("#mainListDiv").toggleClass("show_list");
