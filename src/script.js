cb22ac099856855eedaad0a7f58591b5168a20ef
franciscomvargas <87917356+franciscomvargas@users.noreply.github.com>
2024-02-21 12:23:57 +0000

    UPG: Webpage linking by url query

--- src/script.js
+++ src/script.js
@@ -17,6 +17,36 @@ $( document ).ready(function() {
     $('.sty-home').css('background-color', l1_color); // todo > OnLoad!!
 });
 
+/* ================= URL QUERY ================ */
+let urlQuery = new URLSearchParams(window.location.search);
+urlQuery = String(urlQuery).split('&');
+// console.log("[ URL QUERY ] > " + urlQuery);
+let website_queries = {};
+urlQuery.forEach(element => {
+    _query = String(element).split('=');
+    // console.log("[ QUERY ] > " + _query);
+    website_queries[_query[0].toLowerCase()] = _query[1].toLowerCase();
+});
+console.log("[ QUERY OBJ ] > " + JSON.stringify(website_queries, null, 4));
+
+// Handle Section Query
+if (website_queries.hasOwnProperty('section')) {
+    switch (website_queries['section']) {
+        case 'about':
+            show_about_section();
+            break;
+        case 'services':
+            show_services_section();
+            break;
+        case 'portfolio':
+            show_portfolio_section();
+            break;
+        case 'contact':
+            show_contact_section();
+            break;
+    }
+}
+
 /* ================= THEMES ================ */
 function dark_theme() {
     $('.body').css('background-color', nav_footer_color);
@@ -56,46 +86,62 @@ $('.navTrigger, .navlinks').click(function () {
 
 /* ================= WEBSITE LINKING ================ */
 $('.nav-home, .logo').on( "click", function() {
+    show_home_section();
+})
+function show_home_section() {
     dark_theme();
     // home_replace(true)
     $('.sty-home').css('background-color', l1_color);
     $(nav_sectiom_tags).css('display', 'none');
     $('.section-homepage').css('display', 'block');
     document.title = 'Jumbie';
-})
+}
 
 $('.nav-about').on( "click", function() {
+    show_about_section();
+})
+function show_about_section() {
     light_theme();
     // home_replace(false)
     $('.sty-about').css('background-color', l2_color);
     $(nav_sectiom_tags).css('display', 'none');
     $('.section-about').css('display', 'block');
     document.title = 'Jumbie ~ About';
-})
+}
 
 $('.nav-services').on( "click", function() {
+    show_services_section();
+})
+function show_services_section() {
     light_theme();
     // home_replace(false)
     $('.sty-services').css('background-color', l3_color);
     $(nav_sectiom_tags).css('display', 'none');
     $('.section-services').css('display', 'block');
     document.title = 'Jumbie ~ Services';
-})
+}
 
 $('.nav-portfolio').on( "click", function() {
+    show_portfolio_section();
+})
+function show_portfolio_section() {
     light_theme();
     // home_replace(false)
     $('.sty-portfolio').css('background-color', l4_color);
     $(nav_sectiom_tags).css('display', 'none');
     $('.section-portfolio').css('display', 'block');
     document.title = 'Jumbie ~ Portfolio';
-})
+}
 
 $('.nav-contact, .action-contact').on( "click", function() {
+    show_contact_section();
+})
+function show_contact_section() {
     light_theme();
     // home_replace(false)
     $('.sty-contact').css('background-color', l5_color);
     $(nav_sectiom_tags).css('display', 'none');
     $('.section-contact').css('display', 'block');
     document.title = 'Jumbie ~ Contact';
-})
+
+}
